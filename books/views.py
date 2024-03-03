84f39ab777c85a53913b46b96d3c6bcbf2cfe706
Bharatk003 <bharatkshirsagar003@gmail.com>
2024-03-03 12:10:42 +0530

    added the search option on the homepage use the get_queryset method

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import book
+from django.db.models import Q
 # Create your views here.
 
 
@@ -19,3 +20,14 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status' # new
+
+class SearchResultsListView( ListView):
+    model = book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    # queryset = book.objects.filter(title__icontains='beginners' )
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return (book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query) | Q(price__icontains=query))) 
+     
